df65676b55b12c00f0e2e4a2e86214d03f273f5b
Kendi <muriungikendileila@gmail.com>
2025-02-08 16:50:48 +0300

    adding the tab navigation

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components';
 import {  
@@ -10,6 +13,23 @@ import {
 
 import { theme } from './src/infrastructure/theme';
 import { RestaurantsScreens } from './src/features/restaraunts/screens/restaurant.screen';
+import { SafeArea } from './src/components/utility/safe-area';
+
+
+
+const Tab = createBottomTabNavigator();
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+
+const Map = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -26,7 +46,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreens />
+        <NavigationContainer>
+            <Tab.Navigator>
+              <Tab.Screen name="Restaurants" component={RestaurantsScreens} />
+              <Tab.Screen name="Map" component={Map} />
+              <Tab.Screen name="Settings" component={Settings} />
+            </Tab.Navigator>
+          </NavigationContainer>
       </ThemeProvider>
       
       <ExpoStatusBar style='auto'/>
